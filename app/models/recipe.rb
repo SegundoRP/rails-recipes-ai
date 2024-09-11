c7d3223ea323f44490f3c74e3f0442f80634a417
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-09-10 22:59:20 -0500

    add job instead code

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -37,6 +37,15 @@ class Recipe < ApplicationRecord
 
   def set_content
     RecipeContentJob.perform_later(self)
+    # client = OpenAI::Client.new
+    # chaptgpt_response = client.chat(parameters: {
+    #   model: "gpt-4o-mini",
+    #   messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+    # })
+    # new_content = chaptgpt_response["choices"][0]["message"]["content"]
+
+    # update(content: new_content)
+    # return new_content
   end
 
   def set_photo
